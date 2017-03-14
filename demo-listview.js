ecaffdfdb49410b7d2bd7f7bca262b23f955cd4e
nothing <nocoding@163.com>
2017-03-14 11:05:36 +0800

    touch

--- demo-listview.js
+++ demo-listview.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     AppRegistry,
     StyleSheet,
@@ -6,6 +6,9 @@ import {
     PanResponder,
     View
 } from 'react-native';
+var Dimensions = require('Dimensions');
+var ScreenWidth = Dimensions.get('window').width;
+var ScreenHeight = Dimensions.get('window').height;
 export default class MyTouch extends Component {
     constructor(props, context) {
         super(props, context);
@@ -17,7 +20,8 @@ export default class MyTouch extends Component {
             distant: 0,
         }
     }
-    componentWillMount(){
+
+    componentWillMount() {
         this.gestureHandlers = PanResponder.create({
             onStartShouldSetPanResponder: this._handleStartShouldSetPanResponder.bind(this),
             onMoveShouldSetPanResponder: this._handleMoveShouldSetPanResponder.bind(this),
@@ -27,51 +31,53 @@ export default class MyTouch extends Component {
             onPanResponderTerminate: this._handlePanResponderEnd.bind(this),
         })
     }
-    _handleStartShouldSetPanResponder(e, gestureState){
-        console.log("start" + " " + gestureState.dy);
+
+    _handleStartShouldSetPanResponder(e, gestureState) {
         return gestureState.numberActiveTouches === 2;
     }
 
-    _handleMoveShouldSetPanResponder(e, gestureState){
-        console.log("move start" + " " + gestureState.dy);
-        return gestureState.numberActiveTouches === 2;
+    _handleMoveShouldSetPanResponder(e, gestureState) {
+        return true;
     }
-    _handlePanResponderGrant(e, gestureState){
-        console.log("grant" + " " + gestureState.dy);
+
+    _handlePanResponderGrant(e, gestureState) {
         if (gestureState.numberActiveTouches === 2) {
             this.setState({bg: 'orange'});
         }
     }
-    _handlePanResponderEnd(e, gestureState){
+
+    _handlePanResponderEnd(e, gestureState) {
         this.setState({bg: 'red'});
         console.log(gestureState);
     }
 
-    _handlePanResponderMove(e, gestureState){
-        console.log(gestureState.numberActiveTouches + " " + e.nativeEvent.touches.length);
-        if (gestureState.numberActiveTouches === 2) {
-            this.setState({bg: 'orange'});
-            var dx = Math.abs(e.nativeEvent.touches[0].pageX - e.nativeEvent.touches[1].pageX);
-            var dy = Math.abs(e.nativeEvent.touches[0].pageY - e.nativeEvent.touches[1].pageY);
-            var distant = Math.sqrt(dx*dx + dy*dy);
-            if (distant > this.state.distant) {
-                console.log("bigger");
-            } else {
-                console.log("smaller");
-            }
-            this.setState({distant: distant});
+    _handlePanResponderMove(e, gestureState) {
+        if (gestureState.numberActiveTouches === 1) {
+            // this.setState({bg: 'orange'});
+            // var dx = Math.abs(e.nativeEvent.touches[0].pageX - e.nativeEvent.touches[1].pageX);
+            // var dy = Math.abs(e.nativeEvent.touches[0].pageY - e.nativeEvent.touches[1].pageY);
+            // var distant = Math.sqrt(dx * dx + dy * dy);
+            // if (distant > this.state.distant) {
+            //     console.log("bigger");
+            // } else {
+            //     console.log("smaller");
+            // }
+            console.log("=" + gestureState.dy);
+            this.setState({distant: gestureState.dy});
         }
     }
+
     render() {
         return (
-            <View style={styles.container}>
+            <View>
                 <View
                     {...this.gestureHandlers.panHandlers}
-                    style={[styles.rectBig, {
-                        "backgroundColor": this.state.bg,
-                        "width": this.state.width,
-                        "height": this.state.height
-                    }]}>
+                    style={{
+                        height: ScreenHeight,
+                        width: ScreenWidth,
+                        backgroundColor: this.state.bg,
+                        marginTop: this.state.distant
+                    }}>
                 </View>
             </View>
         );
